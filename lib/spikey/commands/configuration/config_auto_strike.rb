3006c2212f1c538d62aef88299d6c75180f5d30e
Coding Cactus <65423356+Coding-Cactus@users.noreply.github.com>
2022-01-09 17:44:09 +0000

    Make config commands admin only

--- lib/spikey/commands/configuration/config_auto_strike.rb
+++ lib/spikey/commands/configuration/config_auto_strike.rb
@@ -3,6 +3,18 @@ class Spikey
 		embed     = nil
 		server    = event.server
 		server_id = server.id
+
+		unless event.author.defined_permission?(:administrator) || server.owner == event.user
+			return event.send_embed(
+				"",
+				Discordrb::Webhooks::Embed.new(
+					title: "Insufficient Permissions!",
+					description: "You must be an administrator to use the configuration commands.",
+					colour: "cc0000".to_i(16),
+					timestamp: Time.new
+				)
+			)
+		end
 		
 
 		if count == nil || count == "0"
