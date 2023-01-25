1084ae6213b028f3d62ebd779427b556358a9c29
Coding Cactus <65423356+Coding-Cactus@users.noreply.github.com>
2023-01-25 17:04:21 +0000

    🔥 Remove server

--- main.rb
+++ main.rb
@@ -1,14 +1,6 @@
 require "mongo"
-require "sinatra"
 require "discordrb"
 
 require_relative "lib/spikey"
 
-
-set :bind, "0.0.0.0"
-
-get "/" do
-	"hi"
-end
-
-Thread.new { Spikey.new(ENV["token"], ENV["mongouri"]).run }
+Spikey.new(ENV["token"], ENV["mongouri"]).run
